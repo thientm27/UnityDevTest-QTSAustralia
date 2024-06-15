a4af23409670b7a1f6c2b424dc1994bfafa9e0e8
thientm27 <denkhotieu1@gmail.com>
2024-06-15 13:29:21 +0700

    Init

---
+++ Assets/Scripts/Game/PlayerMoveController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class PlayerMoveController : MonoBehaviour
+    {
+        [SerializeField] private float walkingSpeed = 7.5f;
+        [SerializeField] private float runningSpeed = 11.5f;
+        [SerializeField] private float jumpSpeed = 8.0f;
+        [SerializeField] private float gravity = 20.0f;
+        [SerializeField] private Camera playerCamera;
+        [SerializeField] private float lookSpeed = 2.0f;
+        [SerializeField] private float lookXLimit = 45.0f;
+
+        [SerializeField] private CharacterController _playerMoveController;
+        Vector3 moveDirection = Vector3.zero;
+        float rotationX = 0;
+
+        [HideInInspector]
+        public bool canMove = true;
+
+        void Update()
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 right = transform.TransformDirection(Vector3.right);
+            // Press Left Shift to run
+            bool isRunning = Input.GetKey(KeyCode.LeftShift);
+            float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+            float movementDirectionY = moveDirection.y;
+            moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+            if (Input.GetButton("Jump") && canMove && _playerMoveController.isGrounded)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+            else
+            {
+                moveDirection.y = movementDirectionY;
+            }
+
+            // Apply gravity. Gravity is multiplied by deltaTime twice 
+            if (!_playerMoveController.isGrounded)
+            {
+                moveDirection.y -= gravity * Time.deltaTime;
+            }
+
+            // Move the controller
+            _playerMoveController.Move(moveDirection * Time.deltaTime);
+
+            // Player and Camera rotation
+            if (canMove)
+            {
+                rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+                rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+              //  playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+                transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            }
+        }
+
+        public void PauseGame()
+        {
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            canMove = false;
+        }
+
+        public void ResumeGame()
+        {
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            canMove = true;
+        }
+    }
+}
