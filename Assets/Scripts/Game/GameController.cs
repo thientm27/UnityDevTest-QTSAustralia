daa588be552e41a4bbf150ebae0c869448fafdda
thientm27 <denkhotieu1@gmail.com>
2024-06-15 14:08:51 +0700

    Set up obj

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -4,16 +4,6 @@ namespace Game
 {
     public class GameController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+       
     }
 }
---
+++ Assets/Scripts/Game/GameModel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class GameModel : MonoBehaviour
+    {
+        [SerializeField] private float enemyASpawnRateMin;
+        [SerializeField] private float enemyASpawnRateMax;
+
+        [SerializeField] private float enemyBSpawnRateMin;
+        [SerializeField] private float enemyBSpawnRateMax;
+        
+    }
+}
